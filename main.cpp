1c6b1f2319ae0e7198249e312fe4243641f40ded
stc_176-PC\stc_176 <rlatkfhs1@gmail.com>
2018-05-02 15:48:01 +0900

    파일 최초등록

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 2018-05-02 오전 10:52:06
+ * Author : stc_176
+ */ 
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+int main(void)
+{
+	PORTD=0xff;
+	DDRD=0xff;
+
+    /* Replace with your application code */
+    while (1) 
+    {
+		int a;
+		int b;
+		
+		for(int i=0; i<7;i++)
+		{
+			PORTD=~(1<<i);
+			_delay_ms(100);
+		}
+		for(int i=0; i<7;i++)
+		{
+			PORTD=~(0x80>>i);
+			_delay_ms(100);
+		}
+
+
+
+    }
+
+		
+		
+    }
+
+
